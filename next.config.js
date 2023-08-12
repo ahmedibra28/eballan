ebb818441ac2dea819bcd318163a39ee055d6b69
Ahmed <ahmaat19@gmail.com>
2023-08-12 10:32:46 +0300

    updating next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ['ui-avatars.com', 'source.unsplash.com', 'www.worldometers.info', 'upload.wikimedia.org',
+            'b2c-admin-storage.s3-ap-southeast-1.amazonaws.com', 'amazonaws.com'],
+    },
+}
+
+module.exports = nextConfig
