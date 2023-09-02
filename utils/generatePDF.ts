e18f9a3cfa8f3982e881ea2d16b756da2003e89f
Ahmed <ahmaat19@gmail.com>
2023-09-02 13:16:33 +0300

    updating puppeteer path

--- utils/generatePDF.ts
+++ utils/generatePDF.ts
@@ -1,7 +1,10 @@
 import puppeteer from 'puppeteer'
 
 const generatePDF = async (html: any) => {
-  const browser = await puppeteer.launch({ headless: 'new' })
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    executablePath: '/usr/bin/chromium-browser',
+  })
   const page = await browser.newPage()
   await page.setContent(html, { waitUntil: 'networkidle0' })
   const pdf = await page.pdf()
