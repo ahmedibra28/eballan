669255e6819b8b8b3c0285de7e3719c0e6e8f576
Ahmed <ahmaat19@gmail.com>
2023-09-02 13:49:39 +0300

    updating puppeteer excutable path

--- utils/generatePDF.ts
+++ utils/generatePDF.ts
@@ -3,6 +3,7 @@ import puppeteer from 'puppeteer'
 const generatePDF = async (html: any) => {
   const browser = await puppeteer.launch({
     headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
     executablePath: '/snap/bin/chromium',
   })
   const page = await browser.newPage()
